f6fae621ac73f4cec13103bd147a8ad4a5147386
ctochimani <cecilia.sorianoti@udlap.mx>
2024-10-11 15:23:41 -0600

    agregué perfil

---
+++ segura/segura/Perfil.swift
@@ -0,0 +1,19 @@
+//
+//  Perfil.swift
+//  segura
+//
+//  Created by iOS Lab on 11/10/24.
+//
+
+import SwiftUI
+
+struct Perfil: View {
+    var body: some View {
+        Text("Perfil")
+        Image(systemName: "tree")
+    }
+}
+
+#Preview {
+    Perfil()
+}
